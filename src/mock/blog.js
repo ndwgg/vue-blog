de40323abc80ae9883224c0c3445cf01cbfc57c6
蒋伟 <2336365486@qq.com>
2021-05-26 14:00:49 +0800

    更换头像

--- src/mock/blog.js
+++ src/mock/blog.js
@@ -1,25 +1,25 @@
 import Mock from 'mockjs'
 import qs from 'querystring'
 
-Mock.mock("/api/blogtype", 'get', {
+Mock.mock( "/api/blogtype", 'get', {
     code: 200,
     msg: "ok",
-    "data|30-40": [{
+    "data|30-40": [ {
         "id|+1": 1,
         name: "分类-@id",
         "articleCount|0-150": 10,
         "order|+1": 1
-    }]
-})
+    } ]
+} )
 
-Mock.mock(/^\/api\/blog(\?.+)?$/, 'get', function (option) {
-    const query = qs.parse(option.url)
-    return Mock.mock({
+Mock.mock( /^\/api\/blog(\?.+)?$/, 'get', function ( option ) {
+    const query = qs.parse( option.url )
+    return Mock.mock( {
         code: 200,
         msg: "ok",
         data: {
             "total|500-1000": 0,
-            [`rows|${query.limit}`]: [
+            [ `rows|${ query.limit }` ]: [
                 {
                     id: "@guid",
                     title: "@ctitle(10,30)",
@@ -30,15 +30,15 @@ Mock.mock(/^\/api\/blog(\?.+)?$/, 'get', function (option) {
                     },
                     scanNumber: "@natural(1000,5000)",
                     commentNumber: "@natural(100,500)",
-                    "thumb|1": ["@image(180x150,@color ,@cname)", null],
+                    "thumb|1": [ "@image(180x150,@color ,@cname)", null ],
                     createDate: "@time('T')"
                 }
             ]
         }
-    })
-})
+    } )
+} )
 
-Mock.mock(/^\/api\/blog\/[^/]+$/, 'get', {
+Mock.mock( /^\/api\/blog\/[^/]+$/, 'get', {
     code: 0,
     msg: "",
     data: {
@@ -264,9 +264,9 @@ Mock.mock(/^\/api\/blog\/[^/]+$/, 'get', {
     })</code></pre>
     <p>这样一来，该跨域的ajax请求就是一个<em>附带身份凭证的请求</em></p><p>当一个请求需要附带cookie时，无论它是简单请求，还是预检请求，都会在请求头中添加<code>cookie</code>字段</p><p>而服务器响应时，需要明确告知客户端：服务器允许这样的凭据</p><p>告知的方式也非常的简单，只需要在响应头中添加：<code>Access-Control-Allow-Credentials: true</code>即可</p><p>对于一个附带身份凭证的请求，若服务器没有明确告知，浏览器仍然视为跨域被拒绝。</p><p>另外要特别注意的是：<strong>对于附带身份凭证的请求，服务器不得设置 <code>Access-Control-Allow-Origin 的值为*</code></strong>。这就是为什么不推荐使用*的原因</p><h1 id="article-md-title-7">一个额外的补充</h1><p>在跨域访问时，JS只能拿到一些最基本的响应头，如：Cache-Control、Content-Language、Content-Type、Expires、Last-Modified、Pragma，如果要访问其他头，则需要服务器设置本响应头。</p><p><code>Access-Control-Expose-Headers</code>头让服务器把允许浏览器访问的头放入白名单，例如：</p><pre><code>Access-Control-Expose-Headers: authorization, a, b</code></pre><p>这样JS就能够访问指定的响应头了。</p>`,
     },
-})
+} )
 
-Mock.mock("/api/comment", "post", {
+Mock.mock( "/api/comment", "post", {
     code: 0,
     msg: "",
     data: {
@@ -275,15 +275,16 @@ Mock.mock("/api/comment", "post", {
         content: "@cparagraph(1, 10)",
         createDate: Date.now(),
         "avatar|1": [
-            "http://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar6.jpg",
-            "http://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar4.jpg",
-            "http://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar8.jpg",
-            "http://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar2.jpg",
+            "http://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2714399441,791784588&fm=26&gp=0.jpg",
+            "http://img1.baidu.com/it/u=1796842200,2277127495&fm=26&fmt=auto&gp=0.jpg",
+            "http://img2.baidu.com/it/u=3250889597,2466047395&fm=26&fmt=auto&gp=0.jpg",
+            "http://img1.baidu.com/it/u=1212161490,2709179851&fm=26&fmt=auto&gp=0.jpg",
         ],
-    },
-})
 
-Mock.mock(/^\/api\/comment\/?(\?.+)?$/, "get", {
+    },
+} )
+
+Mock.mock( /^\/api\/comment\/?(\?.+)?$/, "get", {
     code: 0,
     msg: "",
     data: {
@@ -299,12 +300,12 @@ Mock.mock(/^\/api\/comment\/?(\?.+)?$/, "get", {
                 },
                 createDate: Date.now(),
                 avatar: "头像地址", "avatar|1": [
-                    "http://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar6.jpg",
-                    "http://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar4.jpg",
-                    "http://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar8.jpg",
-                    "http://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar2.jpg",
+                    "http://dss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2714399441,791784588&fm=26&gp=0.jpg",
+                    "http://img1.baidu.com/it/u=1796842200,2277127495&fm=26&fmt=auto&gp=0.jpg",
+                    "http://img2.baidu.com/it/u=3250889597,2466047395&fm=26&fmt=auto&gp=0.jpg",
+                    "http://img1.baidu.com/it/u=1212161490,2709179851&fm=26&fmt=auto&gp=0.jpg",
                 ]
             }
         ]
     }
-})
+} )
